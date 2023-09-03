7a7efc86a7b3b1010107bb72e35b14d73c8949b1
Kaivela <kaivel100@gmail.com>
2023-09-03 20:25:02 +0200

    v0.7 (no bug) (new verif for no card to draw)

--- lol.js
+++ lol.js
@@ -1,3 +1,18 @@
+// *Tasks TO DO
+//
+
+// *DONE
+//-check comment se comporte la bomb actuellement dans le cas extreme où on jouerait la bombe en derniere carte :
+// Qui fait bien son effet normalement (c'est check)
+//-preventCantDraw by resetting pile before draw
+// on met donc la pile dans le deck en se rappelant du total de la pile pour ajouter des cartes a piocher, si plus de pile a mettre dans leck alors pas de pioche
+
+// *NEW
+// function remindLives (bout de code était répété 3 fois et j'ai sorti la fonction qui peut d'ailleurs s'avérer utile par la suite)
+// global value : savedTotal
+// function remindPile (pour prévenir le fait de piocher undifined)
+// function ResetPile (il fallait update le transferPileToDeck en resetant également le savedTotal)
+
 import Enquirer from "enquirer";
 
 //déclaration de variables globaless
@@ -11,6 +26,7 @@ let playerCount = 0;
 let totalPile = 0;
 let total = 0;
 let reverseOrder = false;
+let savedTotal = 0;
 
 //---------------------------------------------
 
@@ -20,9 +36,7 @@ initHandsAndLives();
 newRound();
 //Tour du joueur courant
 while (!isGameOver()) {
-  console.log(
-    "\n--------------" + "\nJOUEUR " + (currentPlayer + 1) + "\n--------------"
-  );
+  console.log("\n--------------" + "\nJOUEUR " + (currentPlayer + 1) + "\n");
   totalPile = computePile();
   const currentHand = playerHands[currentPlayer];
 
@@ -37,7 +51,7 @@ while (!isGameOver()) {
         playerLives[currentPlayer] +
         "\nle total de la pile revient à 0"
     );
-    transferPileToDeck();
+    resetPile();
     console.log({ total: computePile() });
     nextTurn();
     continue; // redémarre la boucle au début
@@ -86,15 +100,7 @@ while (!isGameOver()) {
             "\nChaque autre joueur perd une vie!\n"
         );
         decreaseOpponentsLives();
-        console.log("***Rappel :");
-        for (let playerIndex = 0; playerIndex < playerCount; playerIndex++) {
-          console.log(
-            "Nombre de vie du joueur " +
-              (playerIndex + 1) +
-              ": " +
-              playerLives[playerIndex]
-          );
-        }
+        remindLives();
         newRound();
       } else {
         playShuffleHands();
@@ -104,27 +110,6 @@ while (!isGameOver()) {
     //si carte joué == bomb
     if (currentPlayedCard == "bomb") {
       playBomb();
-      // for (let playerIndex = 0; playerIndex < playerCount; playerIndex++) {
-      //   if (playerIndex == currentPlayer) continue;
-      //   if (haveZero(currentHand) == true) {
-      //     console.log(
-      //       "le joueur " + (playerIndex + 1) + " se déffausser d'une carte 0"
-      //     );
-      //   }
-
-      //   // manque le code pour que le joueur retire le 0 ciblé de sa main
-      //   // manque le code pour ajouter cette derniere a la pile
-
-      //   // sinon perd une vie
-      //   else {
-      //     console.log(
-      //       "le joueur " +
-      //         (playerIndex + 1) +
-      //         " ne peut se déffausser d'une carte 0\nIl perd donc une vie !"
-      //     );
-      //     playerLives[playerIndex]--;
-      //   }
-      // }
     }
 
     //si carte joué == "+1" ou "+2"
@@ -144,15 +129,7 @@ while (!isGameOver()) {
           "\nChaque autre joueur perd une vie!\n"
       );
       decreaseOpponentsLives();
-      console.log("***Rappel :");
-      for (let playerIndex = 0; playerIndex < playerCount; playerIndex++) {
-        console.log(
-          "Nombre de vie du joueur " +
-            (playerIndex + 1) +
-            ": " +
-            playerLives[playerIndex]
-        );
-      }
+      remindLives();
       newRound();
     }
     console.log({ total: computePile() });
@@ -161,10 +138,28 @@ while (!isGameOver()) {
 }
 //---------------------------------------------
 
+//demande combien de joueurs jouent
+async function askPlayerCount() {
+  const enquirer = new Enquirer();
+  const response = await enquirer.prompt({
+    type: "input",
+    name: "joueurs",
+    message: "combien de joueurs etes vous?",
+  });
+  playerCount = Number(response.joueurs);
+}
+
+//assignation des vies et main pour chaque joueur
+function initHandsAndLives() {
+  for (let x = 0; x < playerCount; x++) {
+    playerHands.push([]);
+    playerLives.push(5);
+  }
+}
+
 //création des cartes + ajout au deck
 function initDeck() {
   for (let deckIndex = 0; deckIndex < 77; deckIndex++) {
-    //deckIndex sera de 77 quand terminé
     if (deckIndex < 8) {
       // 8 cartes : Zéro
       deck.push(0);
@@ -223,14 +218,6 @@ function initDeck() {
   }
 }
 
-//création d'un tableau pour les vies et la main pour chaque joueur
-function initHandsAndLives() {
-  for (let x = 0; x < playerCount; x++) {
-    playerHands.push([]);
-    playerLives.push(5);
-  }
-}
-
 //Nouveau Round
 function newRound() {
   console.log("\nNous débutons un nouveau round");
@@ -242,6 +229,14 @@ function newRound() {
   eachPlayerDrawCards(10);
 }
 
+//vider la main de chaque joueur
+function emptyAllHands() {
+  playerHands = [];
+  for (let x = 0; x < playerCount; x++) {
+    playerHands.push([]);
+  }
+}
+
 //Mélanger le deck
 function shuffleDeck() {
   deck.sort(function () {
@@ -249,8 +244,34 @@ function shuffleDeck() {
   });
 }
 
+// chaque joueur pioche X carte(s)
+function eachPlayerDrawCards(count) {
+  if (count == 1 || count == 2) {
+    console.log(
+      "Chaque joueur pioche " +
+        count +
+        " carte" +
+        (count >= 2 ? "s" : "") +
+        " !"
+    );
+  }
+  for (let x = 0; x < count; x++) {
+    for (let playerIndex = 0; playerIndex < playerCount; playerIndex++) {
+      drawCard(playerIndex);
+    }
+  }
+}
+
 //Piocher une carte
 function drawCard(playerIndex) {
+  if (deck.length == 0) {
+    remindPile();
+    shuffleDeck();
+  }
+  if (deck.length == 0) {
+    console.log("y a plus de cartes... dommage !");
+    return;
+  }
   const cardDrawn = deck.shift();
   playerHands[playerIndex].push(cardDrawn);
 }
@@ -267,7 +288,6 @@ async function selectCard() {
       value: String(index),
     };
   });
-
   const select = await enquirer.prompt({
     type: "select",
     name: "card",
@@ -313,7 +333,7 @@ function canPlay(totalPile, playerHand) {
 function playCard(cardPlayedIndex) {
   let cardPlayed = playerHands[currentPlayer].splice(cardPlayedIndex, 1)[0];
   if (cardPlayed == "= 0" || cardPlayed == "= 10" || cardPlayed == "= 21") {
-    transferPileToDeck();
+    resetPile();
   }
   pile.push(cardPlayed);
 }
@@ -343,7 +363,7 @@ function computePile() {
     }
     total = total + cardValue;
   }
-  return total;
+  return total + savedTotal;
 }
 
 //tour du joueur suivant
@@ -396,62 +416,15 @@ function isGameOver() {
   return gameIsOver;
 }
 
-//vider la main de chaque joueur
-function emptyAllHands() {
-  playerHands = [];
-  for (let x = 0; x < playerCount; x++) {
-    playerHands.push([]);
-  }
-}
-
-// chaque joueur pioche X carte(s)
-function eachPlayerDrawCards(count) {
-  if (count == 1 || count == 2) {
-    console.log(
-      "Chaque joueur pioche " +
-        count +
-        " carte" +
-        (count >= 2 ? "s" : "") +
-        " !"
-    );
-  }
-  for (let x = 0; x < count; x++) {
-    for (let playerIndex = 0; playerIndex < playerCount; playerIndex++) {
-      drawCard(playerIndex);
-    }
-  }
-}
-
-// demande combien de joueurs jouent
-async function askPlayerCount() {
-  const enquirer = new Enquirer();
-  const response = await enquirer.prompt({
-    type: "input",
-    name: "joueurs",
-    message: "combien de joueurs etes vous?",
-  });
-  playerCount = Number(response.joueurs);
-}
-
 //mettre les cartes dans un pack
 function transferHandsToPack() {
   for (let playerIndex = 0; playerIndex < playerCount; playerIndex++) {
-    // console.log({ player: playerIndex + 1 });
-    // console.log({ pack });
-    // console.log({ playerhand: playerHands[playerIndex] });
     console.log(
       "le joueur " + (playerIndex + 1) + " met ses cartes dans le pack"
     );
     pack = pack.concat(playerHands[playerIndex]);
-    // console.log({ pack });
     playerHands[playerIndex] = [];
-    // console.log({ playerhand: playerHands[playerIndex] });
-    // console.log("--------------------");
   }
-  // console.log({ playerHands });
-  // console.log("les mains doivent etre vides !!!");
-  // console.log({ pack });
-  // console.log("le pack doit etre plein !!!");
 }
 
 //Mélanger le pack
@@ -463,45 +436,29 @@ function shufflePack() {
 
 //distribuer le pack en commencant par la gauche
 function dealPack() {
-  // console.log({ shufflePlayer: currentPlayer + 1 });
   let playerIndex = currentPlayer;
   while (pack.length > 0) {
     playerIndex++;
     if (playerIndex > playerCount - 1) {
       playerIndex = 0;
     }
-    // console.log("********************");
-    // console.log({ pack });
     const cardDrawn = pack.shift();
-    // console.log({ player: playerIndex + 1 });
-    // console.log("--------------------");
-    // console.log({ playerhand: playerHands[playerIndex] });
     playerHands[playerIndex].push(cardDrawn);
-    // console.log("--------------------");
-    // console.log({ playerhand: playerHands[playerIndex] });
-    // console.log({ pack });
-    // console.log("********************\n");
   }
-  // console.log({ pack });
-  // console.log("le pack doit etre vide !!!");
-  // console.log({ playerHands });
-  // console.log("les mains doivent etre pleines !!!");
 }
 
+//joue la carte shuffleHands
 function playShuffleHands() {
   transferHandsToPack();
   shufflePack();
-  // console.log("le pack a du être mélangé ?");
-  // console.log({ pack });
-  // console.log("--------------------");
   console.log(
     "le joueur " +
       (currentPlayer + 1) +
-      " distribue le pack en commencant par le joueur à sa gauche"
+      " mélange puis distribue le pack en commencant par le joueur à sa gauche"
   );
   dealPack();
   console.log("le total est remis à 0");
-  transferPileToDeck();
+  resetPile();
 }
 
 //joue la carte bombe
@@ -509,6 +466,7 @@ function playBomb() {
   for (let playerIndex = 0; playerIndex < playerCount; playerIndex++) {
     const currentHand = playerHands[playerIndex];
     if (playerIndex == currentPlayer) continue;
+    console.log({ Player: playerIndex + 1 });
     //si possède une carte 0 alors l'envoie vers la pile
     if (haveZero(currentHand) == true) {
       console.log(
@@ -518,10 +476,8 @@ function playBomb() {
       const cardIndex = currentHand.findIndex(function isZero(card) {
         return card == 0;
       });
-
       // index trouvé --> dégager la carte de sa main avec splice
       playerHands[playerIndex].splice(cardIndex, 1);
-
       // push un 0 dans la pile
       pile.push(0);
     }
@@ -535,6 +491,26 @@ function playBomb() {
       playerLives[playerIndex]--;
     }
   }
+  remindLives();
+  console.log("le total est remis à 0");
+  resetPile();
+}
+
+// parcours la main pour vérifier si celle ci contient une carte 0
+function haveZero(playerHand) {
+  let haveZeroInHand = false;
+  for (let cardIndex = 0; cardIndex < playerHand.length; cardIndex++) {
+    let cardValue = playerHand[cardIndex];
+    if (cardValue == 0) {
+      haveZeroInHand = true;
+    }
+  }
+  console.log({ haveZeroInHand });
+  return haveZeroInHand;
+}
+
+// rappel du nombre de vie
+function remindLives() {
   console.log("***Rappel :");
   for (let playerIndex = 0; playerIndex < playerCount; playerIndex++) {
     console.log(
@@ -544,20 +520,16 @@ function playBomb() {
         playerLives[playerIndex]
     );
   }
-  console.log("le total est remis à 0");
+}
+
+//save current totalPile + transfer pile to deck
+function remindPile() {
+  savedTotal = computePile();
   transferPileToDeck();
 }
 
-// parcours la main pour vérifier si celle ci contient une carte 0
-function haveZero(playerHand) {
-  let haveZeroInHand = false;
-
-  for (let cardIndex = 0; cardIndex < playerHand.length; cardIndex++) {
-    let cardValue = playerHand[cardIndex];
-    if (cardValue == 0) {
-      haveZeroInHand = true;
-    }
-  }
-  console.log({ haveZeroInHand });
-  return haveZeroInHand;
+//reset la pile et le saved total
+function resetPile() {
+  transferPileToDeck();
+  savedTotal = 0;
 }
